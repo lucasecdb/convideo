47e5a178d71303b219b0b3608631b81dfcdbce07
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-08-28 01:14:14 -0300

    fix compile flags

--- lib/ffmpeg/build.sh
+++ lib/ffmpeg/build.sh
@@ -8,17 +8,20 @@ export CFLAGS="${OPTIMIZE} -I${PREFIX}/include/"
 export CPPFLAGS="${OPTIMIZE} -I${PREFIX}/include/"
 export LDFLAGS="${OPTIMIZE} -L${PREFIX}/lib/"
 
-COMMON_FILTERS="aresample scale crop overlay"
-COMMON_DEMUXERS="matroska ogg avi mov flv mpegps image2 mp3 concat"
-COMMON_DECODERS="vp8 vp9 theora \
-  mpeg2video mpeg4 h264 hevc \
-  png mjpeg \
-  vorbis opus \
-  mp3 ac3 aac \
-  ass ssa srt webvtt"
+apt-get update
+apt-get install -qqy pkg-config
 
-MP4_MUXERS="mp4 mp3 null"
-MP4_ENCODERS="libx264 libmp3lame aac"
+COMMON_FILTERS=aresample,scale,crop,overlay
+COMMON_DEMUXERS=matroska,ogg,avi,mov,flv,mpegps,image2,mp3,concat
+COMMON_DECODERS="vp8,vp9,theora,\
+mpeg2video,mpeg4,h264,hevc,\
+png,mjpeg,\
+vorbis,opus,\
+mp3,ac3,aac,\
+ass,ssa,srt,webvtt"
+
+MP4_MUXERS=mp4,mp3,null
+MP4_ENCODERS=libx264,libmp3lame,aac
 
 echo "=========================="
 echo "Compiling libx264"
@@ -77,15 +80,51 @@ echo "=========================="
 (
   cd node_modules/ffmpeg
   EM_PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig emconfigure ./configure \
-    $(FFMPEG_COMMON_ARGS) \
-    $(addprefix --enable-encoder=,$(MP4_ENCODERS)) \
-    $(addprefix --enable-muxer=,$(MP4_MUXERS)) \
+    --cc=emcc \
+    --enable-cross-compile \
+    --target-os=none \
+    --arch=x86 \
+    --disable-runtime-cpudetect \
+    --disable-asm \
+    --disable-fast-unaligned \
+    --disable-pthreads \
+    --disable-w32threads \
+    --disable-os2threads \
+    --disable-debug \
+    --disable-stripping \
+    --disable-all \
+    --enable-ffmpeg \
+    --enable-avcodec \
+    --enable-avformat \
+    --enable-avutil \
+    --enable-swresample \
+    --enable-swscale \
+    --enable-avfilter \
+    --disable-network \
+    --disable-d3d11va \
+    --disable-dxva2 \
+    --disable-vaapi \
+    --disable-vdpau \
+    $(eval echo --enable-decoder={$COMMON_DECODERS}) \
+    $(eval echo --enable-demuxer={$COMMON_DEMUXERS}) \
+    --enable-protocol=file \
+    $(eval echo --enable-filter={$COMMON_FILTERS}) \
+    --disable-bzlib \
+    --disable-iconv \
+    --disable-libxcb \
+    --disable-lzma \
+    --disable-securetransport \
+    --disable-xlib \
+    --disable-zlib \
+    $(eval echo --enable-encoder={$MP4_ENCODERS}) \
+    $(eval echo --enable-muxer={$MP4_MUXERS}) \
     --enable-libmp3lame \
     --enable-gpl \
     --enable-libx264 \
     --extra-cflags="-I ${PREFIX}/include" \
     --extra-ldflags="-L ${PREFIX}/lib"
   emmake make -j8
+  cp ffmpeg ffmpeg.bc
 )
 echo "=========================="
 echo "Compiling ffmpeg done"
@@ -102,10 +141,10 @@ echo "=========================="
     -s INVOKE_RUN=0 \
     -s ALLOW_MEMORY_GROWTH=1 \
     -s 'EXPORT_NAME="ffmpeg"' \
-    -o ffmpeg-mp4.js \
+    -o ffmpeg.js \
     ${PREFIX}/lib/libmp3lame.so \
     ${PREFIX}/lib/libx264.so \
-    node_modules/ffmpeg/ffmpeg
+    node_modules/ffmpeg/ffmpeg.bc
 )
 echo "=========================="
 echo "Generating bidings done"
