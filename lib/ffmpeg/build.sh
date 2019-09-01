21d02d2bcd18c2a52cedd74797ff7e210442d08f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-01 01:29:08 -0300

    add asm.js build option

--- lib/ffmpeg/build.sh
+++ lib/ffmpeg/build.sh
@@ -34,147 +34,150 @@ vp8,\
 vp9,\
 webvtt"
 
-if [ ! $SKIP_BUILD ]; then
-  echo "=========================="
-  echo "Compiling libx264"
-  echo "=========================="
-  (
-    cd node_modules/libx264
-    if ! patch -R -s -f -p1 --dry-run < ../../x264-configure.patch; then
-      patch -p1 < ../../x264-configure.patch
-    fi
-    emconfigure ./configure \
-      --prefix=${PREFIX}/ \
-      --extra-cflags="-Wno-unknown-warning-option" \
-      --host=x86-none-linux \
-      --disable-cli \
-      --enable-shared \
-      --disable-opencl \
-      --disable-thread \
-      --disable-asm \
-      --disable-avs \
-      --disable-swscale \
-      --disable-lavf \
-      --disable-ffms \
-      --disable-gpac \
-      --disable-lsmash
-    emmake make -j8
-    emmake make install
-  )
-  echo "=========================="
-  echo "Compiling libx264 done"
-  echo "=========================="
+echo "=========================="
+echo "Compiling libx264"
+echo "=========================="
+test -n "$SKIP_BUILD" || (
+  cd node_modules/libx264
+  if ! patch -R -s -f -p1 --dry-run < ../../x264-configure.patch; then
+    patch -p1 < ../../x264-configure.patch
+  fi
+  emconfigure ./configure \
+    --prefix=${PREFIX}/ \
+    --extra-cflags="-Wno-unknown-warning-option" \
+    --host=x86-none-linux \
+    --disable-cli \
+    --enable-shared \
+    --disable-opencl \
+    --disable-thread \
+    --disable-asm \
+    --disable-avs \
+    --disable-swscale \
+    --disable-lavf \
+    --disable-ffms \
+    --disable-gpac \
+    --disable-lsmash
+  emmake make -j8
+  emmake make install
+)
+echo "=========================="
+echo "Compiling libx264 done"
+echo "=========================="
 
-  echo "=========================="
-  echo "Compiling lame"
-  echo "=========================="
-  (
-    cd node_modules/libmp3lame
-    emconfigure ./configure \
-      --prefix=${PREFIX}/ \
-      --host=x86-none-linux \
-      --disable-static \
-      \
-      --disable-gtktest \
-      --disable-analyzer-hooks \
-      --disable-decoder \
-      --disable-frontend
-    emmake make -j8
-    emmake make install
-  )
-  echo "=========================="
-  echo "Compiling lame done"
-  echo "=========================="
+echo "=========================="
+echo "Compiling lame"
+echo "=========================="
+test -n "$SKIP_BUILD" || (
+  cd node_modules/libmp3lame
+  emconfigure ./configure \
+    --prefix=${PREFIX}/ \
+    --host=x86-none-linux \
+    --disable-static \
+    \
+    --disable-gtktest \
+    --disable-analyzer-hooks \
+    --disable-decoder \
+    --disable-frontend
+  emmake make -j8
+  emmake make install
+)
+echo "=========================="
+echo "Compiling lame done"
+echo "=========================="
 
-  echo "=========================="
-  echo "Compiling zlib"
-  echo "=========================="
-  (
-    cd node_modules/zlib
-    emconfigure ./configure \
-      --prefix=${PREFIX}/
-    emmake make -j8
-    emmake make install
-  )
-  echo "=========================="
-  echo "Compiling zlib done"
-  echo "=========================="
+echo "=========================="
+echo "Compiling zlib"
+echo "=========================="
+test -n "$SKIP_BUILD" || (
+  cd node_modules/zlib
+  emconfigure ./configure \
+    --prefix=${PREFIX}/
+  emmake make -j8
+  emmake make install
+)
+echo "=========================="
+echo "Compiling zlib done"
+echo "=========================="
 
-  echo "=========================="
-  echo "Compiling ffmpeg"
-  echo "=========================="
-  (
-    cd node_modules/ffmpeg
-    EM_PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig emconfigure ./configure \
-      --cc=emcc \
-      --enable-cross-compile \
-      --target-os=none \
-      --arch=x86 \
-      --disable-runtime-cpudetect \
-      --disable-asm \
-      --disable-fast-unaligned \
-      --disable-pthreads \
-      --disable-w32threads \
-      --disable-os2threads \
-      --disable-debug \
-      --disable-stripping \
-      --disable-all \
-      --enable-ffmpeg \
-      --enable-avcodec \
-      --enable-avformat \
-      --enable-avutil \
-      --enable-swresample \
-      --enable-swscale \
-      --enable-avfilter \
-      --disable-network \
-      --disable-d3d11va \
-      --disable-dxva2 \
-      --disable-vaapi \
-      --disable-vdpau \
-      $(eval echo --enable-decoder={$DECODERS}) \
-      $(eval echo --enable-encoder={$ENCODERS}) \
-      $(eval echo --enable-demuxer={$DEMUXERS}) \
-      $(eval echo --enable-muxer={$MUXERS}) \
-      --enable-protocol=file \
-      $(eval echo --enable-filter={$FILTERS}) \
-      --disable-bzlib \
-      --disable-iconv \
-      --disable-libxcb \
-      --disable-lzma \
-      --disable-securetransport \
-      --disable-xlib \
-      --enable-libmp3lame \
-      --enable-gpl \
-      --enable-libx264 \
-      --enable-zlib \
-      --extra-cflags="-I ${PREFIX}/include" \
-      --extra-ldflags="-L ${PREFIX}/lib"
-    emmake make -j8
-    cp ffmpeg ffmpeg.bc
-  )
-  echo "=========================="
-  echo "Compiling ffmpeg done"
-  echo "=========================="
-fi
+echo "=========================="
+echo "Compiling ffmpeg"
+echo "=========================="
+test -n "$SKIP_BUILD" || (
+  cd node_modules/ffmpeg
+  EM_PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig emconfigure ./configure \
+    --cc=emcc \
+    --enable-cross-compile \
+    --target-os=none \
+    --arch=x86 \
+    --disable-runtime-cpudetect \
+    --disable-asm \
+    --disable-fast-unaligned \
+    --disable-pthreads \
+    --disable-w32threads \
+    --disable-os2threads \
+    --disable-debug \
+    --disable-stripping \
+    --disable-all \
+    --enable-ffmpeg \
+    --enable-avcodec \
+    --enable-avformat \
+    --enable-avutil \
+    --enable-swresample \
+    --enable-swscale \
+    --enable-avfilter \
+    --disable-network \
+    --disable-d3d11va \
+    --disable-dxva2 \
+    --disable-vaapi \
+    --disable-vdpau \
+    $(eval echo --enable-decoder={$DECODERS}) \
+    $(eval echo --enable-encoder={$ENCODERS}) \
+    $(eval echo --enable-demuxer={$DEMUXERS}) \
+    $(eval echo --enable-muxer={$MUXERS}) \
+    --enable-protocol=file \
+    $(eval echo --enable-filter={$FILTERS}) \
+    --disable-bzlib \
+    --disable-iconv \
+    --disable-libxcb \
+    --disable-lzma \
+    --disable-securetransport \
+    --disable-xlib \
+    --enable-libmp3lame \
+    --enable-gpl \
+    --enable-libx264 \
+    --enable-zlib \
+    --extra-cflags="-I ${PREFIX}/include" \
+    --extra-ldflags="-L ${PREFIX}/lib"
+  emmake make -j8
+  cp ffmpeg ffmpeg.bc
+)
+echo "=========================="
+echo "Compiling ffmpeg done"
+echo "=========================="
+
+EMSCRIPTEN_COMMON_ARGS="--bind \
+  ${OPTIMIZE} \
+  --closure 1 \
+  -s MODULARIZE=1 \
+  -s INVOKE_RUN=0 \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -s EXPORT_NAME=\"ffmpeg\" \
+  --std=c++11 \
+  -I node_modules/ffmpeg
+  ${PREFIX}/lib/libmp3lame.so \
+  ${PREFIX}/lib/libx264.so \
+  node_modules/ffmpeg/ffmpeg.bc \
+  ffmpeg.cpp"
 
 echo "=========================="
 echo "Generating bidings"
 echo "=========================="
 (
-  emcc \
-    --bind \
-    ${OPTIMIZE} \
-    --closure 1 \
-    -s MODULARIZE=1 \
-    -s INVOKE_RUN=0 \
-    -s ALLOW_MEMORY_GROWTH=1 \
-    -s 'EXPORT_NAME="ffmpeg"' \
-    -o ffmpeg.js \
-    --std=c++11 \
-    ${PREFIX}/lib/libmp3lame.so \
-    ${PREFIX}/lib/libx264.so \
-    node_modules/ffmpeg/ffmpeg.bc \
-    ffmpeg.cpp
+  emcc $EMSCRIPTEN_COMMON_ARGS -o ffmpeg.js
+
+  if [ $ASM ]; then
+    emcc $EMSCRIPTEN_COMMON_ARGS -s WASM=0 -o ffmpeg.asm.js
+  fi
 )
 echo "=========================="
 echo "Generating bidings done"
