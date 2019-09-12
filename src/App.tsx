19b17dd6fa17a1147cea595725d9fcee06067cd4
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-12 14:17:41 -0300

    add missing packages

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ const App: React.FC = () => {
       }
     }
   }, [])
+
   const handleFile = async (inputFile: File) => {
     if (inputFile.size > FILE_SIZE_LIMIT) {
       setShowFileSizeError(true)
