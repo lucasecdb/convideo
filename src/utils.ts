62ad0f8042eaa8b782d8a1bdd9612b1ade30c077
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-12 19:00:03 -0300

    fix file not downloading on firefox

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,12 @@ export const downloadFile = (filename: string, data: ArrayBuffer) => {
   const anchor = document.createElement('a')
   anchor.download = filename
   anchor.href = url
+
+  document.body.appendChild(anchor)
+
   anchor.click()
+
+  document.body.removeChild(anchor)
 
   URL.revokeObjectURL(url)
 }
